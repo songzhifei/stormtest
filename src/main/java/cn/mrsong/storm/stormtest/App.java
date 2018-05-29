25f23061f50b873e016d51efa72e799174f2042d
zhifeisong <zhifeisong@163.com>
2018-05-29 17:29:31 +0800

    storm test

---
+++ src/main/java/cn/mrsong/storm/stormtest/App.java
@@ -0,0 +1,13 @@
+package cn.mrsong.storm.stormtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
